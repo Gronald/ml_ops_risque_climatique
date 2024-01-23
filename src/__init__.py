4be28fb1eccdf5d2debcefd08412f804fd39919c
MIGLIETTI <xavier.miglietti@credit-agricole-sa.fr>
2024-01-24 00:01:29 +0100

    suppression init

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-json_file_path = "results_query.json"
-
-
-if not os.path.exists(os.path.join("data","logs",json_file_path)):
-    with open(os.path.join("data","logs",json_file_path),"w") as json_file:
-        json.dump([],json_file)
