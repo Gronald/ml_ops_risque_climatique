abda42ea87186910f7233f518f49d9ebc253e8ee
MIGLIETTI <xavier.miglietti@credit-agricole-sa.fr>
2024-01-24 08:34:41 +0100

    changement repertoir logs

--- src/main.py
+++ src/main.py
@@ -86,17 +86,13 @@ def authenticate_user(username: str, password: str):
 
 
 def read_json() :
-    with open(os.path.join("data","logs",json_file_name), 'r') as log_file:
+    with open(os.path.join("src","data","logs",json_file_name), 'r') as log_file:
          return json.load(log_file)
     
 def create_json() :
-    with open(os.path.join("data","logs",json_file_name),"w") as json_file:
+    with open(os.path.join("src","data","logs",json_file_name),"w") as json_file:
         json.dump([],json_file)
 
-
-def save_json() :
-    with open(os.path.join("data","logs",json_file_name),"w") as json_file:
-        json.dump([],json_file)
 
 @app.post("/predict")
 def predict(params : ModelParams,user: dict = Depends(get_current_user)):
@@ -122,7 +118,7 @@ def predict(params : ModelParams,user: dict = Depends(get_current_user)):
     json_file.append(log_info)
 
     #Stockage des logs dans un fichier JSON
-    with open(os.path.join("data","logs",json_file_name), 'w') as updated_file:
+    with open(os.path.join("src","data","logs",json_file_name), 'w') as updated_file:
         json.dump(json_file,updated_file)
 
     return pred
